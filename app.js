92c35cd7e565112beaa83b0e2016c71ba4336d49
Wangj-da <wangj_da@sina.com>
2019-02-28 18:37:10 +0800

    小程序

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+  //系统事件
+  onLaunch: function () {//小程序初始化事件
+    var that=this;
+    //调用API从本地缓存中获取数据
+    that.curid = wx.getStorageSync('curid') || that.curid;//API：获取本地缓存，若不存在设置为全局属性
+    that.setlocal('curid', that.curid);//调用全局方法
   },
-  globalData: {
-    userInfo: null
-  }
+
+  /*******************************************************/
+
+  //自定义全局方法
+  setlocal:function(id,val){
+    wx.setStorageSync(id, val);//API：设置本地缓存
+  },
+  //自定义全局属性
+  curid:"CN101010100",
+  version:"1.0"
 })
